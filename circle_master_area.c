e4dca68c1a94242ae36d7b9f98a0cc4f2d8b2125
abhinav3102 <abhinavsrinivas310@gmail.com>
2022-06-08 18:11:49 +0530

    Added circumference feature

--- circle_master_area.c
+++ circle_master_area.c
@@ -5,11 +5,13 @@ int main(){
 
 	int radius;
 	float area;
+	float circumference;
 
 	printf("Enter radius of the circle: ");
 	scanf("%d", &radius);
 
 	area = PI * radius * radius;
+	circumference = 2*PI*radius;
 	printf("Area of the circle = %0.3f \n", area);
 
 	return 0;
